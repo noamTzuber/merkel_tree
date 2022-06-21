aca2c77a7b449037f38039f19d0eb824d2efa04d
noamTzuber <noamtooby@gmail.com>
2022-06-21 20:42:48 +0300

    done

--- merkle.py
+++ merkle.py
@@ -1,5 +1,4 @@
-# Itay Shwartz 318528171
-# Noam Tzuberi 313374837
+# Itay Shwartz, 318528171, Noam Tzuberi, 313374837
 
 import hashlib
 import math
@@ -13,49 +12,62 @@ from cryptography.hazmat.primitives import serialization
 
 
 def add_node(list,leaf):
-
     list.append(hashlib.sha256(str(leaf).encode('utf-8')).hexdigest())
 
 
 def find_root(list, first, end):
+    # if the list is empty we return
     if len(list) == 0:
         return
+    # if we reach to point that first = end that's mean we look to a single object in array - se we return it.
     if first == end:
         return list[first]
+
+    # we find the biggest power of two that contain in our list size
     x = math.floor(math.log((end-first+1), 2))
+    # if 2 power x is the size of the array we do x--
     if math.pow(2, x) == end-first+1:
         x = x - 1
+    # find the root of the left and right and do hash on both to return the root
     left = find_root(list, first,int(first + math.pow(2, x)-1))
     right = find_root(list,int( first + math.pow(2, x)) , end)
     return hashlib.sha256(str(left + right).encode('utf-8')).hexdigest()
 
-def find_proof(list, i, j, index, result_list):
+def find_proof(list, i, j, index, result_list,side):
+    # if we reach to single leaf in the array - i == j then we add the side of it and the leaf.
     if i == j:
-        result_list.append(list[index])
+        result_list.append(str(side)+list[index])
         return
+    # we find the biggest power of two that contain in our list size
     x = math.floor(math.log((j - i + 1), 2))
+    # if 2 power x is the size of the array we do x--
     if math.pow(2, x) == j - i + 1:
         x = x - 1
+    # if the leaf that we search is in left side we find it first and than we find it first and than append
+    # the root of the right side.
     if i <= index <= math.pow(2, x)+i-1:
-        find_proof(list, i, int(math.pow(2, x)+i-1), index, result_list)
-        result_list.append(find_root(list, int(math.pow(2, x)+i), j))
+        find_proof(list, i, int(math.pow(2, x)+i-1), index, result_list,0)
+        result_list.append('1'+find_root(list, int(math.pow(2, x)+i), j))
     else:
-        find_proof(list,int(math.pow(2, x) + i), j, index, result_list)
-        result_list.append(find_root(list, i, int(math.pow(2, x)+i-1)))
+        find_proof(list,int(math.pow(2, x) + i), j, index, result_list,1)
+        result_list.append('0'+find_root(list, i, int(math.pow(2, x)+i-1)))
 
 def print_the_proof(res_list_proof):
     print_list=" ".join(str(x) for x in res_list_proof)
     print(print_list)
 
 def proof_of_inclusion(leaf,root,proof):
-
     tmp = hashlib.sha256(str(leaf).encode('utf-8')).hexdigest()
-    for x in proof:
-        tmp = hashlib.sha256(str(tmp+x).encode('utf-8')).hexdigest()
+    # we do hash on pairs by the 0 or 1 that appears in the first bit of the hash to decide who come left or right.
+    for x in proof[1:]:
+        if x[0]=='1':
+            tmp = hashlib.sha256(str(tmp+x[1:]).encode('utf-8')).hexdigest()
+        else:
+            tmp = hashlib.sha256(str(x[1:]+tmp).encode('utf-8')).hexdigest()
     print(root == tmp)
 
 def genarate_keys():
-
+    # generate key's by the algorithm of RSA
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
@@ -76,6 +88,7 @@ def genarate_keys():
     print(pem_public.decode('utf=8'))
 
 def signature_root(key,root):
+    # we signature the root with the key that we given.
     while True:
         tmp =input()
         key = key + "\n" + tmp
@@ -95,6 +108,7 @@ def signature_root(key,root):
     print(base64.b64encode(signature).decode())
 
 def verify_sign(public_key):
+    # we get the rest of the public key by the loop
     while True:
         tmp = input()
         public_key = public_key + "\n" + tmp
@@ -104,8 +118,8 @@ def verify_sign(public_key):
     txt = input().encode()
     public_key = serialization.load_pem_public_key(public_key.encode(),backend=default_backend())
 
+    # we try to verify. if it true then we print true. if we reach to except then it failed so we return false.
     try:
-
         public_key.verify(
             base64.b64decode(signature),
             txt,
@@ -119,16 +133,14 @@ def verify_sign(public_key):
     except:
         print(False)
 
-
-
-
-
 if __name__ == '__main__':
     list = []
 
     while True:
+
         res_list_proof = []
         parameter = input()
+        # we split the input by spaces
         action_value = parameter.split()
 
         if len(action_value) == 0:
@@ -137,13 +149,19 @@ if __name__ == '__main__':
         choose = action_value[0]
         try:
             if choose == '1':
+                # we return the return the string from the list
                 var =" ".join(str(x) for x in action_value[1:])
                 add_node(list, var)
             elif choose == '2':
-                print(find_root(list, 0, len(list)-1))
+                res=(find_root(list, 0, len(list)-1))
+                if res is None:
+                    print('')
+                    continue
+                print(res)
             elif choose == '3':
+                # we create the list of proof and the root, and return both.
                 res_list_proof.append(find_root(list, 0, len(list)-1))
-                find_proof(list, 0, len(list)-1, int(action_value[1]), res_list_proof)
+                find_proof(list, 0, len(list)-1, int(action_value[1]), res_list_proof,0)
                 print_the_proof(res_list_proof)
             elif choose == '4':
                 proof_of_inclusion(action_value[1],action_value[2],action_value[3:])
